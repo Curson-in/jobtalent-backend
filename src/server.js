5a327a10a7976f34e7e2d9a11020262617b4acda
Shrawan <shrawanre07@gmail.com>
2026-01-09 22:27:08 +0530

    Initial backend setup

--- src/server.js
+++ src/server.js
@@ -48,7 +48,7 @@ const app = express();
 ===================================================== */
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://curson-one.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
